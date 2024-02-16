14a6cade90ace0ae1800b8c419bdd6fcff3d3c74
Vikashy05 <152557272+Vikashy05@users.noreply.github.com>
2024-02-17 00:09:35 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,12 @@
+package TASK1;
+
+/**
+ * Q1
+ */
+public class Q1 {
+
+    public static void main(String[] args) {
+        int n=(int)(Math.random()*100);
+        System.out.println(n);
+    }
+}
